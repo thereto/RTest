f3772b7101bf5662b063f7f5b64c55f979d0251d
thereto <827742363@qq.com>
2026-04-09 11:01:00 +0800

    Update logger.py

--- common/logger.py
+++ common/logger.py
@@ -1,1 +1,14 @@
 # 日志封装
+import logging
+
+def get_logger():
+    logger = logging.getLogger("auto_test")
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        ch = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+    return logger
