89b171b30e94715000aa7e0b52af47253876a062
thereto <827742363@qq.com>
2026-04-09 11:02:08 +0800

    Update test_login.py

--- testcases/test_login.py
+++ testcases/test_login.py
@@ -1,1 +1,17 @@
 
+import pytest
+from common.request_util import RequestUtil
+from common.assertion import Assertion
+from utils.file_util import load_yaml
+
+data = load_yaml("data/login_data.yaml")
+
+@pytest.mark.parametrize("case", data)
+def test_api(case):
+    response = RequestUtil.send_request(
+        method=case["method"],
+        url=case["url"],
+        params=case.get("params")
+    )
+
+    Assertion.assert_code(response, case["expected_code"])
