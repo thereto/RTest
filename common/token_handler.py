7ed89c3e08116b1d3de7d286cc57b8286df615f1
thereto <827742363@qq.com>
2026-04-09 11:28:14 +0800

    Update token_handler.py

--- common/token_handler.py
+++ common/token_handler.py
@@ -6,25 +6,31 @@ from common.logger import logger
 class TokenHandler:
     token = None
 
+    # @classmethod
+    # def get_token(cls):
+    #     if cls.token:
+    #         return cls.token
+
+    #     logger.info("开始获取token...")
+
+    #     response = RequestUtil.send_request(
+    #         method=LOGIN_INFO["method"],
+    #         url=LOGIN_INFO["url"],
+    #         json=LOGIN_INFO["data"],
+    #         need_token=False
+    #     )
+
+    #     # httpbin没有token，这里模拟
+    #     cls.token = "mock_token_123456"
+
+    #     logger.info(f"获取token成功: {cls.token}")
+    #     return cls.token
+
     @classmethod
     def get_token(cls):
         if cls.token:
             return cls.token
-
-        logger.info("开始获取token...")
-
-        response = RequestUtil.send_request(
-            method=LOGIN_INFO["method"],
-            url=LOGIN_INFO["url"],
-            json=LOGIN_INFO["data"],
-            need_token=False
-        )
-
-        # httpbin没有token，这里模拟
-        cls.token = "mock_token_123456"
-
-        logger.info(f"获取token成功: {cls.token}")
-        return cls.token
+        return cls.refresh_token()
 
     @classmethod
     def refresh_token(cls):
