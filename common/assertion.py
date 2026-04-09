d70a79f69c65b1a53c3af0d8750e8b92053805d5
thereto <827742363@qq.com>
2026-04-09 11:01:19 +0800

    Update assertion.py

--- common/assertion.py
+++ common/assertion.py
@@ -1,1 +1,10 @@
 # 断言封装
+class Assertion:
+
+    @staticmethod
+    def assert_code(response, expected_code):
+        assert response.status_code == expected_code
+
+    @staticmethod
+    def assert_in_text(response, expected_text):
+        assert expected_text in response.text
