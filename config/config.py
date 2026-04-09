306a2bb33a00a68eddbb4581913ec0e0529966cb
thereto <827742363@qq.com>
2026-04-09 11:06:08 +0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,1 +1,10 @@
+import yaml
 
+def load_config():
+    with open("config/config.yaml", encoding="utf-8") as f:
+        return yaml.safe_load(f)
+
+CONFIG = load_config()
+BASE_URL = CONFIG["base_url"]
+DEFAULT_HEADERS = CONFIG["default_headers"]
+LOGIN_INFO = CONFIG["login"]
