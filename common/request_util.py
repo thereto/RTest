c10a2be6afc25a630b4ac1dbfa9ff71cff6959be
thereto <827742363@qq.com>
2026-04-09 11:00:19 +0800

    Add RequestUtil class for HTTP request handling

--- common/request_util.py
+++ common/request_util.py
@@ -1,1 +1,12 @@
 # 请求封装
+
+import requests
+from config.config import BASE_URL
+
+class RequestUtil:
+
+    @staticmethod
+    def send_request(method, url, **kwargs):
+        full_url = BASE_URL + url
+        response = requests.request(method, full_url, **kwargs)
+        return response
