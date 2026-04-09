30b7c4d45a10cc0f222fde8e240865caddf1e1d5
thereto <827742363@qq.com>
2026-04-09 11:31:19 +0800

    Update test_api.py

--- testcases/test_api.py
+++ testcases/test_api.py
@@ -1,17 +1,21 @@
-
 import pytest
+import allure
 from common.request_util import RequestUtil
 from common.assertion import Assertion
 from utils.file_util import load_yaml
 
 cases = load_yaml("data/login_data.yaml")
 
+@allure.feature("接口测试")
 @pytest.mark.parametrize("case", cases)
 def test_api(case):
-    response = RequestUtil.send_request(
-        method=case["method"],
-        url=case["url"],
-        params=case.get("params")
-    )
 
-    Assertion.assert_code(response, case["expected_code"])
+    with allure.step(f"执行用例: {case['name']}"):
+        response = RequestUtil.send_request(
+            method=case["method"],
+            url=case["url"],
+            params=case.get("params")
+        )
+
+    with allure.step("断言状态码"):
+        Assertion.assert_code(response, case["expected_code"])
